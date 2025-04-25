2c47bed7c404aace6b362f9fd648a35767151289
Aleksandr Omelucik <bladewizard@mail.ru>
2025-04-25 09:00:26 +0300

    add resuts search

--- index.js
+++ index.js
@@ -17,17 +17,18 @@ if (i) {
 
 const search = () => {
     const inputNode = document.getElementById("input").value;
-    params.set("t", inputNode);
+    params.set("s", inputNode);
     params.set("apikey","beca027f")
-    const apikey = "beca027f";
+    //const apikey = "beca027f";
     
     console.log(params.toString());
-    fetch(`http://www.omdbapi.com/?${apikey}&${params.toString()}`)
+    fetch(`http://www.omdbapi.com/?${params.toString()}`)
     .then (response => response.json())
     .then (json => {
         console.log(json);
         result(json);
     })
+    .catch(error => console.error('Ошибка:', error));
     window.history.replaceState({}, '', `${window.location.pathname}?${params}`);
     console.log(window.location.href);
 
@@ -36,24 +37,36 @@ const result = (data) => {
     const contentNode = document.getElementById("content");
     contentNode.innerHTML = "";
     if (data.Response === "True") {
-        const addLi = document.createElement("li")
-        contentNode.appendChild(addLi)
+            data.Search.forEach(item => {
+                
+                const addLi = document.createElement("li")
+                contentNode.appendChild(addLi)
+                addLi.classList = "contentLi";
+
+                const imgWrapper = document.createElement("div")
+                addLi.appendChild(imgWrapper)
+                imgWrapper.classList = "imgWrapper"
     
-        const img = document.createElement("img")
-        img.src = data.Poster;
-        addLi.appendChild(img)
+                const img = document.createElement("img")
+                img.src = item.Poster;
+                imgWrapper.appendChild(img)
+                img.classList ="contentImg";
     
-        const h2 = document.createElement("h2")
-        h2.textContent = data.Title
-        addLi.appendChild(h2)
+                const h2 = document.createElement("h2")
+                h2.textContent = item.Title
+                addLi.appendChild(h2)
+                h2.classList = "contentH2";
     
-        const p1 = document.createElement("p")
-        p1.textContent = `Год: ${data.Year}`;
-        addLi.appendChild(p1)
+                const p1 = document.createElement("p")
+                p1.textContent = `Год: ${item.Year}`;
+                addLi.appendChild(p1)
+                p1.classList = "contentP1";
     
-        const p2 = document.createElement("P")
-        p2.textContent = data.Type;
-        addLi.appendChild(p2)
+                const p2 = document.createElement("P")
+                p2.textContent = item.Type;
+                addLi.appendChild(p2)
+                p2.classList = "contentP2";
+            });
 
 
     } else {
